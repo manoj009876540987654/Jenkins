a54eb2a51a87d57e24a0ecd3bd821626b739ba57
manoj009876540987654 <144827758+manoj009876540987654@users.noreply.github.com>
2025-11-14 12:17:23 +0530

    Update Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_4uL2T1OLz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '71mpnddn02lnu0om25ovqu3ft0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
